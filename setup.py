0cee3fa037bf612ade887021ac5e0f9a7add40b7
Andre Veit <andre.veit@dhauz.com>
2022-05-16 16:30:11 -0300

    fixing env vars workflow

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="av-tweet-ingestion",
-    version="1.0.2",
+    version="1.0.3",
     description="Ingests tweets using Twitter's RecentAPI.",
     long_description=README,
     long_description_content_type="text/markdown",
